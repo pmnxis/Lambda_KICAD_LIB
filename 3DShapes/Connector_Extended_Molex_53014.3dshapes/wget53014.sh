21a96775657dba75f0fc2a00af3f4fa1fb98aa8c
pmnxis <pmnxis@gmail.com>
2020-07-17 00:21:58 +0900

    temporary 53014 data

---
+++ 3DShapes/Connector_Extended_Molex_53014.3dshapes/wget53014.sh
@@ -0,0 +1,17 @@
+mkdir -p 53014stp
+cd 53014stp
+wget https://www.molex.com/pdm_docs/stp/53014-0210_stp.zip
+wget https://www.molex.com/pdm_docs/stp/53014-0310_stp.zip
+wget https://www.molex.com/pdm_docs/stp/53014-0410_stp.zip
+wget https://www.molex.com/pdm_docs/stp/53014-0510_stp.zip
+wget https://www.molex.com/pdm_docs/stp/53014-0610_stp.zip
+wget https://www.molex.com/pdm_docs/stp/53014-0710_stp.zip
+wget https://www.molex.com/pdm_docs/stp/53014-0810_stp.zip
+wget https://www.molex.com/pdm_docs/stp/53014-0910_stp.zip
+wget https://www.molex.com/pdm_docs/stp/53014-1010_stp.zip
+wget https://www.molex.com/pdm_docs/stp/53014-1110_stp.zip
+wget https://www.molex.com/pdm_docs/stp/53014-1210_stp.zip
+wget https://www.molex.com/pdm_docs/stp/53014-1310_stp.zip
+wget https://www.molex.com/pdm_docs/stp/53014-1410_stp.zip
+wget https://www.molex.com/pdm_docs/stp/53014-1510_stp.zip
+
